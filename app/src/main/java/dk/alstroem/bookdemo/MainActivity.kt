a4e891cfbbbacdd0af415498995ad8328c83642b
Andreas <alstroem1992@gmail.com>
2022-03-10 23:58:54 +0100

    Unused imports removed

--- app/src/main/java/dk/alstroem/bookdemo/MainActivity.kt
+++ app/src/main/java/dk/alstroem/bookdemo/MainActivity.kt
@@ -3,22 +3,13 @@ package dk.alstroem.bookdemo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
 import dk.alstroem.navigation.lib.NavManager
 import dk.alstroem.theme.BookDemoTheme
-import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.flow.launchIn
-import kotlinx.coroutines.flow.onEach
 import javax.inject.Inject
 
 @AndroidEntryPoint
