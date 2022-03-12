969a76fa4afb4c706bba0b10fb7cc85b02ac1be4
Andreas <alstroem1992@gmail.com>
2022-03-12 21:04:11 +0100

    Simple LazyColumn added

--- listnames-ui/src/main/java/dk/alstroem/listnames_ui/ListNames.kt
+++ listnames-ui/src/main/java/dk/alstroem/listnames_ui/ListNames.kt
@@ -1,14 +1,28 @@
 package dk.alstroem.listnames_ui
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
 import dk.alstroem.theme.BookDemoTheme
 
 @Composable
 fun ListNames(navController: NavController) {
-    Greeting(name = "Book demo")
+    ListNamesScreen()
+}
+
+@Composable
+private fun ListNamesScreen(
+    modifier: Modifier = Modifier
+) {
+    LazyColumn(modifier = modifier.fillMaxSize()) {
+        item {
+            Greeting(name = "Book Demo")
+        }
+    }
 }
 
 @Composable
