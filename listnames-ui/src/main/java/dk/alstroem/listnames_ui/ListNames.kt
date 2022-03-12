5496d10c1a1e43011c10dce1dd0704b68683c88f
Andreas <alstroem1992@gmail.com>
2022-03-12 20:19:33 +0100

    ListNames modules added

---
+++ listnames-ui/src/main/java/dk/alstroem/listnames_ui/ListNames.kt
@@ -0,0 +1,9 @@
+package dk.alstroem.listnames_ui
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun ListNames(navController: NavController) {
+
+}
