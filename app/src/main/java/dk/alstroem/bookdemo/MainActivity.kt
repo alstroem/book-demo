e271af422ed784bef1064f5f1466189b705f4f7d
Andreas <alstroem1992@gmail.com>
2022-03-12 20:50:25 +0100

    Updated navigation setup

--- app/src/main/java/dk/alstroem/bookdemo/MainActivity.kt
+++ app/src/main/java/dk/alstroem/bookdemo/MainActivity.kt
@@ -3,45 +3,22 @@ package dk.alstroem.bookdemo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.rememberNavController
 import dagger.hilt.android.AndroidEntryPoint
-import dk.alstroem.navigation.lib.NavManager
 import dk.alstroem.theme.BookDemoTheme
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-
-    @Inject
-    lateinit var navManager: NavManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             BookDemoTheme {
                 val navController = rememberNavController()
-
                 BookNavGraph(
-                    navManager = navManager,
                     navController = navController
                 )
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    BookDemoTheme {
-        Greeting("Android")
     }
 }
--- listnames-ui/src/main/java/dk/alstroem/listnames_ui/ListNames.kt
+++ listnames-ui/src/main/java/dk/alstroem/listnames_ui/ListNames.kt
@@ -1,9 +1,25 @@
 package dk.alstroem.listnames_ui
 
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
+import dk.alstroem.theme.BookDemoTheme
 
 @Composable
 fun ListNames(navController: NavController) {
+    Greeting(name = "Book demo")
+}
 
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    BookDemoTheme {
+        Greeting("Android")
+    }
 }
