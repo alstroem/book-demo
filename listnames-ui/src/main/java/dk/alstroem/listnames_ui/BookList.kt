476ff3831a643353c5bcb8beb54ae02be498ab33
Andreas <alstroem1992@gmail.com>
2022-03-20 23:41:08 +0100

    Book image loading

--- listnames-ui/src/main/java/dk/alstroem/listnames_ui/BookList.kt
+++ listnames-ui/src/main/java/dk/alstroem/listnames_ui/BookList.kt
@@ -1,17 +1,34 @@
 package dk.alstroem.listnames_ui
 
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.GridCells
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyGridScope
+import androidx.compose.foundation.lazy.LazyItemScope
+import androidx.compose.foundation.lazy.LazyVerticalGrid
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.paging.compose.LazyPagingItems
 import androidx.paging.compose.collectAsLazyPagingItems
 import androidx.paging.compose.items
+import coil.compose.AsyncImage
 import dk.alstroem.lists_domain.model.Book
 
 @Composable
@@ -36,7 +53,12 @@ private fun BookListScreen(
         viewModel.getPageFlow(encodedName)
     }.collectAsLazyPagingItems()
 
-    LazyColumn {
+    LazyVerticalGrid(
+        cells = GridCells.Adaptive(minSize = 160.dp),
+        contentPadding = PaddingValues(16.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
         items(bookItems) { book ->
             if (book != null) {
                 BookItem(book = book)
@@ -50,10 +72,19 @@ fun BookItem(
     book: Book,
     modifier: Modifier = Modifier
 ) {
-    Card(
-        modifier = modifier.fillMaxWidth(),
-        containerColor = MaterialTheme.colorScheme.surfaceVariant
-    ) {
-        Text(text = book.title)
+    Card {
+        AsyncImage(model = book.bookImage, contentDescription = "Book image", modifier = modifier
+            .fillMaxWidth()
+            .aspectRatio(0.66f))
+    }
+}
+
+@ExperimentalFoundationApi
+fun <T: Any> LazyGridScope.items(
+    lazyPagingItems: LazyPagingItems<T>,
+    itemContent: @Composable LazyItemScope.(value: T?) -> Unit
+) {
+    items(lazyPagingItems.itemCount) { index ->
+        itemContent(lazyPagingItems[index])
     }
 }
