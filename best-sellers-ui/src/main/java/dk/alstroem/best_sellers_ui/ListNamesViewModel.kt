f389de444a2ba07dba2d79844d78b86777dac83d
Andreas <alstroem1992@gmail.com>
2022-04-16 14:21:06 +0200

    Unused import

--- best-sellers-ui/src/main/java/dk/alstroem/best_sellers_ui/ListNamesViewModel.kt
+++ best-sellers-ui/src/main/java/dk/alstroem/best_sellers_ui/ListNamesViewModel.kt
@@ -13,7 +13,6 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
